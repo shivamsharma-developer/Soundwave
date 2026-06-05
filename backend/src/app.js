f16c0297f99da7bdc1d807e5e81a38d893d4b909
Shivam Sharma <shivamsharma99151@gmail.com>
2026-06-05 14:03:44 +0530

    fix cors remove trailing slash

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const musicRoutes = require('./routes/music.routes');
 const app = express();
 
 app.use(cors({
-  origin: 'https://soundwave-ebon-chi.vercel.app/', // your React app URL
+  origin: 'https://soundwave-ebon-chi.vercel.app',
   credentials: true               // allows cookies to be sent
 }));
 
